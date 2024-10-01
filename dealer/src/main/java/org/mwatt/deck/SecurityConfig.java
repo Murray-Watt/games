0e709d7ac87e4c0a6e536f395719d4dc54430479
murray-watt <murray-watt@hotmail.com>
2024-10-01 17:06:20 -0600

    JWT dependencies

--- dealer/src/main/java/org/mwatt/deck/SecurityConfig.java
+++ dealer/src/main/java/org/mwatt/deck/SecurityConfig.java
@@ -13,7 +13,6 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -35,10 +34,6 @@ public class SecurityConfig {
         http.headers(headers ->
                 headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin));
         http.csrf(AbstractHttpConfigurer::disable);
-
-
-
-
 
         return http.build();
     }
